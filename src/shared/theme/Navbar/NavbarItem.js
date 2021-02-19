ab790e512babf12efd5bb3293dca74d69f2bd3d0
NightThinker <travel_special@hotmail.com>
2021-02-19 15:52:13 +0700

    navbar

--- src/shared/theme/Navbar/NavbarItem.js
+++ src/shared/theme/Navbar/NavbarItem.js
@@ -1,8 +1,8 @@
 import { NavLink } from 'react-router-dom'
 
 const NavBarItem = ({ to, name }) => {
-  return (<li className='hover:bg-blue-500 hover:text-white '>
-    <NavLink className='flex m-0 py-1 px-5 rounded-sm' exact to={to} activeClassName='bg-blue-500 text-white'>{name}</NavLink>
+  return (<li className=' hover:text-blue-500 '>
+    <NavLink className='flex m-0 py-1 px-5 rounded-sm' exact to={to} activeClassName='border-b-4 border-blue-500 font-medium text-blue-500'>{name}</NavLink>
   </li>)
 }
 
