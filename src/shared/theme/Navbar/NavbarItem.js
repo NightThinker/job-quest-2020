cd287c2583649a75965a86e380c297279e26f571
NightThinker <travel_special@hotmail.com>
2021-02-19 15:35:02 +0700

    hover

--- src/shared/theme/Navbar/NavbarItem.js
+++ src/shared/theme/Navbar/NavbarItem.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
 const NavBarItem = ({ to, name }) => {
-  return (<li className='py-1 px-5 hover:border-b-2 hover:border-blue-500'>
+  return (<li className='py-1 px-5 rounded-sm hover:bg-blue-500 hover:text-white'>
     <Link to={to}>{name}</Link>
   </li>)
 }
