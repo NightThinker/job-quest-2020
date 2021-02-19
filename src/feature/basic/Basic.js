8872a0e095488cabab9d52114d4d8d84da255202
NightThinker <travel_special@hotmail.com>
2021-02-19 13:18:05 +0700

    shift

--- src/feature/basic/Basic.js
+++ src/feature/basic/Basic.js
@@ -40,12 +40,26 @@ const Basic = () => {
     }
   };
 
+  const shift = (arr, position, number) => {
+    if (position === 'right') {
+      for (var i = 0; i < number; i++) {
+        arr.unshift(arr.pop());
+      }
+      return arr
+    }
+    for (var i = 0; i < number; i++) {
+      arr.push(arr.shift())
+    }
+    return arr
+  }
 
 
-  console.log('fib', fib(12))
-  console.log('fizzBuzz', fizzBuzz(45))
+
+  console.log('fib', fib(1), fib(3), fib(12))
+  console.log('shift', shift(['john', 'jane', 'sarah', 'alex'], 'left', 2), shift([1, 2, 3, 4, 5], 'right', 3))
   console.log('secondMax', secondMax([2, 3, 4, 5]), secondMax([9, 2, 21, 21]), secondMax([4, 4, 4, 4]), secondMax([4123]))
   // console.log('secondMax error', secondMax([]))
+  console.log('fizzBuzz', fizzBuzz(21), fizzBuzz(25), fizzBuzz(45))
   return <div><ul><li><p>Fibonacci Sequence</p></li></ul></div>
 }
 
