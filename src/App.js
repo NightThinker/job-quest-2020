a9da86e58f30c51afcf5bc825d8138022fb88969
NightThinker <travel_special@hotmail.com>
2021-02-19 18:15:17 +0700

    change routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
+import { Suspense } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
-import Home from './feature/home/Home'
-import Marathon from './feature/marathon/Marathon'
-import Basic from './feature/basic/Basic'
-import Joke from './feature/joke/Joke'
-import EditJoke from './feature/joke/editJoke/EditJoke'
-
+import { routes } from './shared/components/routes/routes'
 import Navbar from './shared/theme/Navbar/Navbar'
 
 import './App.css';
@@ -19,31 +14,14 @@ import './shared/styles/main.css'
 function App() {
   return (
     <Router>
-      <div>
-
+      <Suspense fallback={<div>Loading....</div>}>
         <Navbar />
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/marathon">
-            <Marathon />
-          </Route>
-          <Route path="/joke/:id">
-            <EditJoke />
-          </Route>
-          <Route path="/joke">
-            <Joke />
-          </Route>
-          <Route path="/basic">
-            <Basic />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-
+          {routes.map((route, i) => (
+            <Route exact key={route.path} {...route} />
+          ))}
         </Switch>
-      </div>
+      </Suspense>
     </Router>
   );
 }
